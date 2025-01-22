e93d9e267fbc449449ff3398fb3c7e4190246dfc
angelsunil06 <angelmarysunil2028@cs.sjcetpalai.ac.in>
2025-01-22 16:29:58 +0530

    Add files via upload

---
+++ sizeofbytes.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+   printf("size of integer=%ld",sizeof(int));
+   printf("\nsize of long integer=%ld",sizeof(long int));
+   return 0;
+}
+
---
+++ sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  int number1,number2;
+  int sum;
+  printf("\nenter two integer numbers");
+  scanf("%d%d",&number1,&number2);
+  sum=number1+number2;
+  printf("sum=%d+%d=%d\n",number1,number2,sum);
+return 0;
+}
